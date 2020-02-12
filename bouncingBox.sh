3b29a0f23ed468fd2e11ddb782571525ec50834e
Aaron <agingalus613@gmail.com>
2020-02-12 12:41:52 -0800

    added rebound

--- bouncingBox.sh
+++ bouncingBox.sh
@@ -24,3 +24,27 @@ do
 	((HIGHT-=2))
 	sleep .2
 done
+
+for(( i=0; i <23; i++ ))
+do
+	clear
+	tput cup $HIGHT $WIDTH
+	((WIDTH))
+	((HIGHT--))
+	echo -e "|___________|"
+	tput cup $HIGHT $WIDTH
+	((WIDTH))
+	((HIGHT--))
+	
+	echo -e "|           |"
+	tput cup $HIGHT $WIDTH
+	((WIDTH))
+	((HIGHT--))
+	echo -e "|           |"
+	tput cup $HIGHT $WIDTH
+	echo -e "_____________"
+	((WIDTH++))
+	((HIGHT+=2))
+	sleep .2
+done
+clear
