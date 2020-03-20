6b5db690f69925d06509823b19f5d5cef0265881
Aaron <agingalus613@gmail.com>
2020-03-19 19:33:41 -0700

    starting madalib

---
+++ madalib.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+myvar=[]
+read -a myvar
+#read -p -a "what is you favorte word  " myvar
+echo "your faveorte word is $(myvar[0]) and $myvar[1]" 
+echo $myvar
+
+adj1="";
+adj2="";
+adj3="";
+adj4="";
+
+echo "It was the dayes of the virus of 2020 when all other $adj1 people where
+		cowering in there $plNoun1. These days where some of the most $adj2 times in history
+		Many people were $verb to the stores to buy all of the $shopping they couold find.
+		The $adj3 Presedednt of the United States of Amarica had the idea to ship 
+		all of the infected people to $properNoun. No one sure would happen to the
+		world, it was a  $adj4 world. Until one crazy scientest $name develped a cure.
+		They relized when you combine $fruit with $candy you get a magical potion.
+		It is said to cure all that ails a person. It was know as the $randPotion.
+		It even curred the $bodyPart of the most famous person in the world $name2.
+		Some say that this story will be witten down in the holy scriptures of $holyBook
+		and studyed for years to come. Others say a new religion call $noun2. 
+		As of the year 2025 we know that peopel and $plAnimal alike have been $adverb 
+		flocking to it. This was a truely $adj time to live though..."
+		
